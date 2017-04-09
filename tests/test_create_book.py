89b07f335765e193957c518dda8c3388a3d2268d
Olena Piankova <el.piankova@gmail.com>
2017-04-09 15:32:08 +0300

    Added pamamenrization for test of book creation

--- tests/test_create_book.py
+++ tests/test_create_book.py
@@ -1,12 +1,19 @@
+import pytest
 from models.book import Book
 
+test_data = [
+    Book(title='0', author='1'),
+    Book(title="New book", author="Super author")
+]
 
-def test_book_creation(app):
-    book = Book(title="New book", author="Super author")
+
+@pytest.mark.parametrize('book', test_data)
+def test_book_creation(app, book):
     response = app.create_object(book)
     # Verification
     assert response.status_code == 201
     assert response.json() == book.get_dict_with_id()
+
 
 def test_book_creation_no_author(app):
     book = Book(title="New book")
